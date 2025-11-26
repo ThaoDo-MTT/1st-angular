0b81a76361498fc5d085c447edc1ad39887db649
ThaoBean <thaobean1701@gmail.com>
2025-11-26 13:51:11 +0700

    route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../shared/header/header';
-import { BodyTodo } from '../shared/body-todo/body-todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, BodyTodo],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
